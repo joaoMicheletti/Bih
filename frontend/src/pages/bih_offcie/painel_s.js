1554c42c731ddc6a81ffbb5a5abb5ff4fb987c34
João Victor <jv604014@gmail.com>
2022-09-27 20:38:52 -0300

    ajustes na home
    ajustes na home

--- frontend/src/pages/bih_offcie/painel_s.js
+++ frontend/src/pages/bih_offcie/painel_s.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style_login.css';
-import {Link, Navigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Logo from '../assets/Logo.jpg';
 
 function Painel_s(){
