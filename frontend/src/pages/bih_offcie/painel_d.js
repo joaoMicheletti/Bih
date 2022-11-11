eee47b408d420612a2425ab08157fbab7c58a2fe
João Micheletti <jv604014@gmail.com>
2022-11-10 21:03:33 -0300

    responsividade da home + get na propaganda
    foi ajustado nesse commit a responsividade da home junto a request (GET)  para buscar a propaganda

--- frontend/src/pages/bih_offcie/painel_d.js
+++ frontend/src/pages/bih_offcie/painel_d.js
@@ -24,8 +24,8 @@ function Painel_d(){
             <header className='Cabeçalho'>
                 <img className='Logo' src={Logo} alt='Logo'/>
                 <nav className='Menu'>
-                    <Link to='/p_salgados'>P_Salgados : </Link>
-                    <Link to='/p_cadastro_itens'>Cadastrar : </Link>
+                    <Link to='/p_salgados'>P_Salgados  </Link>
+                    <Link to='/p_cadastro_itens'>Cadastrar  </Link>
                     <Link to=''>Log_out</Link> 
                 </nav>
             </header>
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import Logo from '../assets/Logo.jpg';
 import './style_home.css';
+import Api from '../../services/api';
 
 function Home(){
+    const [itens, setItens] = useState([]);
+    useEffect(() => {
+        Api.get('/index_prop')
+        .then((Response) => {
+            setItens(Response.data)
+                    
+        })
+        .catch(() => {
+            console.log('erro')
+        })
+    }, [])
+    console.log(itens)
     return(
         <div className='Home_Container' id='Home_Container'>
 
@@ -15,19 +28,27 @@ function Home(){
                     <Link to='/login' >Login</Link>
 
                 </nav>
-            </header>
+            </header> 
 
-            <div className='Conteudo' id='Conteudo'>
-                <div className='Txt_Box'>
-                    <p>Testo propaganda para as datas comemorativas
-                        dia dos namorados, maes, pai, avos, etc....
-                    </p>
-                    <button className='Btn'><Link to='/loja'>Nossa Loja</Link></button>
-                </div>
-                <div className='Img_Box' id='Img_Box'>
-                    <img src={Logo} alt='Imagem da propaganda'/>
-                </div>
-            </div>
+            {itens.map((iten, key) => {
+                const url ='http://localhost:3001/files/';
+
+                return(
+                    <div className='Conteudo' id='Conteudo'>
+                
+                        <div key={iten.id} className='Txt_Box'>
+                            <p>{iten.Texto}</p>
+                            <button className='Btn'><Link to='/loja'>Nossa Loja</Link></button>
+                        </div>
+
+                        <div className='Img_Box' id='Img_Box'>
+                            <img src={url + iten.img_propaganda} alt='Imagem da propaganda'/>
+                        </div>
+                    </div>
+                );
+                })}
+            
+
         </div>
     );
 }
