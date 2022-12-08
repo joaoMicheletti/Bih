69525cec15c73a476c69f61381baa8e9075ba4c6
João Micheletti <jv604014@gmail.com>
2022-12-08 19:11:08 -0300

    carrinho
    Iniciamos o desenvolvimento do carrinho !

--- frontend/src/pages/loja/carrinho.js
+++ frontend/src/pages/loja/carrinho.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {ImExit} from 'react-icons/im';
 import Logo from '../loja/../assets/Logo.jpg';
 import './style_loja.css';
-//import Api from '../../services/api';
+import Api from '../../services/api';
 
 function Carrinho(){
+    //const url = 'http://localhost:3001/files/';
+    const [Today, setToday] = useState(''); 
+    useEffect(() => {
+        Api.get('/carrinho_index_d')
+        .then((Response) => {
+            setToday(Response.data);
+            console.log(Response);
+        }).catch(() => {
+            console.log('Erro');
+        })
+
+    }, []);
+
+    const Enviar = async () => {
+        console.log('Enviar');
+    };
+    const Cancelar = async () => {
+        console.log('Cancelar');
+    };
 
     return(
         <div className='Loja_Container'>
@@ -13,13 +32,32 @@ function Carrinho(){
                 <img className='Logo' src={Logo} alt="Logo"/>
                 <nav className='Menu'>
                     <Link  to="/" >Home</Link>
+                    <Link to='/loja'>Doces</Link>
                     <Link to="/salgados">Salgados</Link>
                     <Link to='/exit'><ImExit/></Link>
                 </nav>
             </header>
 
             <div id='Carrinho_Container'>
-                <h1>carrinho</h1>
+                <div id='Pedidos_hoje'>
+                    <h3>Adicionados Hoje</h3>
+                    <ul>
+                        <li>
+                            <img src={Logo} alt='logo'/>
+                            <p>Name:</p>
+                            <p>Description:</p>
+                            <p>Quantidade:</p>
+                            <p>Preço:</p>
+                            <button onClick={Enviar} >Comprar</button>
+                            <button onClick={Cancelar}>Cancelar</button>
+
+                        </li>
+                    </ul>
+                </div>
+                <hr/>
+                <div id='Pedidos_Finalizados'>
+                    <h3>Pedidos finalizados</h3>
+                </div>
             </div>
                 
         </div>
