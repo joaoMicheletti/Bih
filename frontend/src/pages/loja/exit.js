0589e157fc60eaf39016df230264763ca9d3a9aa
João Micheletti <jv604014@gmail.com>
2022-12-21 21:26:52 -0300

    log out
    exit

--- frontend/src/pages/loja/exit.js
+++ frontend/src/pages/loja/exit.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {AiOutlineShoppingCart} from 'react-icons/ai';
 import Logo from '../loja/../assets/Logo.jpg';
 import './style_loja.css';
 //import Api from '../../services/api';
 
 function Exit(){
+    const History = useNavigate();
+    const Exit = () => {
+        localStorage.removeItem('user');
+        console.log('exit');
+        History('/');
+    };
+    const No_Exit = () => {
+        console.log('no exit');
+        History('/loja');
+    };
 
     return(
         <div className='Loja_Container'>
@@ -22,8 +32,8 @@ function Exit(){
                 <h2>Tem certeza que gostaria de sair .</h2>
                 
                 <div id='Button_Container'>
-                    <button id='BTN_Exit' >Sim</button>
-                    <button id='BTN_Exit' >Não</button>
+                    <button id='BTN_Exit' onClick={Exit} >Sim</button>
+                    <button id='BTN_Exit' onClick={No_Exit} >Não</button>
                 </div>
 
             </div>
