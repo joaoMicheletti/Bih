634d9ad9ca6d97e3bde301ea3d4ef2c865fabd1a
João Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2023-02-27 22:13:51 -0300

    ainda no  frete
    fop ajustada algumas funçoẽs  e estabelecido o valor de 1,50 para cada km  de distancia para a entrega

--- frontend/src/pages/loja/confirm.js
+++ frontend/src/pages/loja/confirm.js
@@ -22,54 +22,16 @@ function Confirm(){
         })
 
     }, []);
-
+    console.log(Pedido_D)
     const [Name, setName] = useState();
     const [Cidade, setCidade] = useState();
     const [Endereco, setEndereco] = useState();
     const [Cep, setCep] = useState();
+    const [Tot, setTot] = useState();
     const [Pagamento, setPagamento] = useState();
-
-    function Comprar(e){
-        e.preventDefault();
-        console.log(Name);
-        console.log(Cidade);
-        if(Name === undefined){
-            alert('Preencha o campo Nome...')
-        } else if (Cidade === undefined){
-            alert('Preencha o campo "Cidade"...');
-        } else if(Endereco === undefined){
-            alert('Preencha o campo ""Endereço"...');
-        } else if(Cep === undefined){
-            alert('Preencha o campo Cep');
-        } else if(Cep.length < 8 || Cep.length > 8){
-            alert('Cep Invalido');
-        } else if(Pagamento === undefined) {
-            alert('Preencha o campo Forma de Pagamneto');
-        };
-        console.log(Pagamento)
-
-        //decrementando o estoque apos efetuar a compra
-        /*const Estoque = iten.estoque;
-        const Decrement_estoque = parseInt(iten.estoque, 10) - parseInt(iten.quantidade);
-        const up_doce = {
-            Name,
-            Estoque,
-            Decrement_estoque
-        };
-        const Up_Estoque_Doce = await Api.put('/estoque_d', up_doce);
-       console.log(Up_Estoque_Doce);*/
-
-       // enviando pedido para a cozinha
-       //const response = await Api.post('/carrinho_pedido');
-    };
-
-
-    function Cancelar_Pedido(e){
-        e.preventDefault();
-        localStorage.removeItem('prod_id');
-        alert('Pedido cancelado');
-        History('/loja');
-    }
+    
+    
+    
     return(
         <div id="Confirm_Container">
             <header id="Confirm_Header">
@@ -133,6 +95,7 @@ function Confirm(){
 
                         <div id="Buttons">
                             {Pedido_D.map((elent, keyy) => {
+                                
 
                                 const Frete = async (e) => {
                                     e.preventDefault();
@@ -152,25 +115,75 @@ function Confirm(){
                                     const Data = {
                                         Cep,
                                     };
+                                    //buscando nobak a distancia em km e atibuindo para cada km o valor de 1,50$;
                                     async function Destance(){
-                                        await Api.post("/frete_calc", Data)
+                                        
+                                        const Res = await Api.post("/frete_calc", Data)
                                         .then(Km => {
-                                            console.log(Km);
-                                        }) .catch(err => {
-                                            console.log(err);
+                                            const frete = parseInt(Km.data);
+                                            console.log(frete);
+                                            const preco = parseFloat(elent.preço);
+                                            const quanti = parseFloat(elent.quantidade);
+                                            const Total = (preco + quanti) + frete * 1.50;
+                                            setTot(Total);
+                                            
+                                            document.querySelector("#Produto").innerHTML = `Valor do produto : ${elent.preço}R$`;
+                                            document.querySelector("#Quantidadee").innerHTML = `Quantidade = ${elent.quantidade}`;
+                                            document.querySelector("#Frete").innerHTML = `Valor do Frete = ${parseFloat(frete * 1.50)} R$ `;
+                                            document.querySelector("#Total").innerHTML = `TOTAL ==== ${parseFloat(Total)} R$`;
+
+                                        }) .catch(err =>{
+                                            alert("Erro interno. tente novamente mais tarde!");
+                                            console.log('erro');
                                         })
                                     }                        
                                     
                                     Destance();
 
-
-
-                                    document.querySelector("#Produto").innerHTML = `Valor do produto : ${elent.preço}R$`;
-                                    document.querySelector("#Quantidadee").innerHTML = `Quantidade : ${elent.quantidade}`;
-                                    document.querySelector("#Frete").innerHTML = `Valor do Frete  `;
-                                    document.querySelector("#Total").innerHTML = '....';
-
                                 };
+                                function Comprar(e){
+                                    e.preventDefault();
+                                    console.log(Name);
+                                    console.log(Cidade);
+                                    if(Name === undefined){
+                                        alert('Preencha o campo Nome...')
+                                    } else if (Cidade === undefined){
+                                        alert('Preencha o campo "Cidade"...');
+                                    } else if(Endereco === undefined){
+                                        alert('Preencha o campo ""Endereço"...');
+                                    } else if(Cep === undefined){
+                                        alert('Preencha o campo Cep');
+                                    } else if(Cep.length < 8 || Cep.length > 8){
+                                        alert('Cep Invalido');
+                                    } else if(Pagamento === undefined) {
+                                        alert('Preencha o campo Forma de Pagamneto');
+                                    };
+                                    console.log(Pagamento)
+                                    console.log(Tot);
+                                    console.log(Pedido_D);
+                            
+                                    //decrementando o estoque apos efetuar a compra
+                                    /*const Estoque = iten.estoque;
+                                    const Decrement_estoque = parseInt(iten.estoque, 10) - parseInt(iten.quantidade);
+                                    const up_doce = {
+                                        Name,
+                                        Estoque,
+                                        Decrement_estoque
+                                    };
+                                    const Up_Estoque_Doce = await Api.put('/estoque_d', up_doce);
+                                   console.log(Up_Estoque_Doce);*/
+                            
+                                   // enviando pedido para a cozinha
+                                   //const response = await Api.post('/carrinho_pedido');
+                                };
+                            
+                            
+                                function Cancelar_Pedido(e){
+                                    e.preventDefault();
+                                    localStorage.removeItem('prod_id');
+                                    alert('Pedido cancelado');
+                                    History('/loja');
+                                }
                                 return(
                                     <div keyy={elent.id}>
                                         <button id="Solicitar" type="submit" onClick={Frete} >Solicitar Valor do frete</button>
@@ -180,42 +193,48 @@ function Confirm(){
                                 
                                         <p id="Produto"></p>
                                         <br/>
+                                        <p id="Await"></p>
                                         <p id="Quantidadee"></p>
                                         <br/>
+                                        <h4 id="Erro"></h4>
                                         <p id="Frete"></p>
                                         <br/>
                                         <p id="Total" ></p>
                                 
                                         </div>
                                         <br/>
+
+                                                    <p id="F_pagamento">Formas de pagamento.</p>
+                                        <p>Cartão</p>
+                                        <p>Pix</p>
+                                        <p>Dinheiro</p>
+                                        <br/>
+                                        <p id="Warning">Caso Sejá (Dinheiro) envie-nos o valor que possui em mãos, 
+                                        para providenciarmos o seu troco, caso necessário </p>
+                                        <br/>
+                                        <p id='Exemplo'>""Exemplo: Dinheiro 150""</p>
+                                        <br/>
+                                        
+                                        <p>Forma de Pagamento</p>
+                                        <input type='text' 
+                                        placeholder="Cartão, Pix, Dinheiro 150"
+                                        onChange={(e) => setPagamento(e.target.value)}></input>
+                                        <br/>
+                                            
+                                        <br/>
+                                        <button id="Confirmar" type="submit" onClick={Comprar} >Confirmar Pedido</button>
+                                        <br/>
+                                        <br/>
+                                        <button id="Cancelar" type='submit'onClick={Cancelar_Pedido} >Cancelar Pedido</button>
+                                        
+
                                     </div>
 
                                 )
                                     
                             })}
 
-                            <p id="F_pagamento">Formas de pagamento.</p>
-                            <p>Cartão</p>
-                            <p>Pix</p>
-                            <p>Dinheiro</p>
-                            <br/>
-                            <p id="Warning">Caso Sejá (Dinheiro) envie-nos o valor que possui em mãos, 
-                            para providenciarmos o seu troco, caso necessário </p>
-                            <br/>
-                            <p id='Exemplo'>""Exemplo: Dinheiro 150""</p>
-                            <br/>
                             
-                            <p>Forma de Pagamento</p>
-                            <input type='text' 
-                            placeholder="Cartão, Pix, Dinheiro 150"
-                            onChange={(e) => setPagamento(e.target.value)}></input>
-                            <br/>
-                                
-                            <br/>
-                            <button id="Confirmar" type="submit" onClick={Comprar} >Confirmar Pedido</button>
-                            <br/>
-                            <br/>
-                            <button id="Cancelar" type='submit'onClick={Cancelar_Pedido} >Cancelar Pedido</button>
                         </div>
                     </form>
                 </div>
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,7 @@ const api = axios.create({
 
 const api = axios.create({
     baseURL: 'http://localhost:3001',
-    timeout: 6000,
+    timeout: 10000,
     headers: {'Content-Type': 'application/json; charset=utf-8'}
   });
 
