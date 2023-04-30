4cf29083fc5ef8fcfd078042b37d763171e0e092
João Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2023-04-29 22:50:01 -0300

    ajustes nos componentes da pagina 404
    foi ajustada a posição de alguns elementos na pagina e ajustes no css

--- frontend/src/pages/404/notfound.js
+++ frontend/src/pages/404/notfound.js
@@ -6,16 +6,20 @@ import Logo from '../assets/Logo.jpg'
 function NotFound(){
     return(
         <div id='NotFaundContainer'>
+            
 
             <div id='Content'>
 
-                <Link id='lk'  to='/loja'>HOME</Link>
+                
                 <img id='Logo_img' src={Logo} alt='logo'/>
 
-                <p id='Code'>4  0   4</p>
+                <p id='Code'></p>
                 
 
                 <p id='Sorry'>Sorry, page notfound!</p>
+                <p id='Return' >Return to Home</p> 
+                <Link id='Linkk'  to='/loja'>Click Here</Link>
+                
 
             </div>
             
